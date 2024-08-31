10f2cdb246ba0963aac5c69fc974fb1d43d3dfa7
Amin Mirzaei <aminmirzaei.og@gmail.com>
2024-08-31 06:07:10 +0330

    - Create BaseSystem Enum

--- GetWeather/Weather.vb
+++ GetWeather/Weather.vb
@@ -1,3 +1,14 @@
-﻿Public Class Weather
+﻿Public Enum BaseSystem
+    Yahoo
+    Accuweather
+    Underground
+    Pollen
+    Irimo
+    Euronews
+End Enum
+
+Public Class Weather
+
+    Private url As String = ""
 
 End Class
--- Meteorology/Program.vb
+++ Meteorology/Program.vb
@@ -1,9 +1,11 @@
 ﻿Imports System.Net.Http
 
+
+
 Module Program
 
     Sub Main()
-
+        System.Diagnostics.Process.Start("https://www.accuweather.com/fa/ir/tehran/210841/weather-forecast/210841")
     End Sub
 
 End Module
